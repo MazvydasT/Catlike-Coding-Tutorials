8adaf56d86c3d79108b4527f98c7edbb3a88fa9b
Maz T <mazvydas.tadaravicius@gmail.com>
2025-08-26 23:56:35 +0100

    Fixes

--- Bevy/CatlikeCodingTutorials/examples/Procedural Meshes/Creating a Mesh/main.rs
+++ Bevy/CatlikeCodingTutorials/examples/Procedural Meshes/Creating a Mesh/main.rs
@@ -1,5 +1,9 @@
 use bevy::prelude::*;
 
+#[path = "../../../src/utils.rs"]
+mod utils;
+use utils::*;
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
@@ -13,7 +17,7 @@ fn startup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let sphere = meshes.add(Sphere::new(0.5).mesh().ico(4).unwrap());
+    let quad = meshes.add(Sphere::new(0.5).mesh().ico(4).unwrap());
 
     let white = materials.add(StandardMaterial {
         base_color: Color::WHITE,
@@ -22,7 +26,7 @@ fn startup(
     });
 
     commands.spawn((
-        Mesh3d(sphere.clone()),
+        Mesh3d(quad.clone()),
         MeshMaterial3d(white.clone()),
         Transform::from_translation(Vec3::ZERO),
     ));
@@ -32,16 +36,19 @@ fn startup(
             color: Color::WHITE,
             ..Default::default()
         },
-        Transform::from_translation(Vec3::new(0.0, 3.0, 0.0)).looking_at(Vec3::ZERO, Vec3::Y),
-        //.with_rotation(Quat::from_rotation_x(50.0))
-        //.with_rotation(Quat::from_rotation_y(-30.0)),
+        Transform::from_rotation(Quat::from_euler(
+            EulerRot::ZXY,
+            deg2rad(50.),
+            deg2rad(-30.),
+            0.,
+        )),
     ));
 
     commands.spawn((
         Camera3d::default(),
-        Transform::from_translation(Vec3::new(0.5, 0.5, -1.0)).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_translation(Vec3::new(0., 0., 1.)),
         Projection::from(PerspectiveProjection {
-            fov: 60.,
+            fov: deg2rad(60.),
             near: 0.3,
             far: 1000.,
             ..Default::default()
---
+++ Bevy/CatlikeCodingTutorials/src/utils.rs
@@ -0,0 +1,5 @@
+use std::f32::consts::PI;
+
+pub fn deg2rad(deg: f32) -> f32 {
+    deg * PI / 180.
+}
